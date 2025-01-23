d795ab2c5586524f01b3e39633f5ba8f19fc48bf
Nikoleta Evtimova <nevtimova98@gmail.com>
2025-01-23 21:59:13 +0200

    Delete Python_Advanced/Multidimensional Lists - Exercise 1 directory

--- Python_Advanced/Multidimensional Lists - Exercise 1/diagonal_difference.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-
-matrix = [list(map(int, input().split())) for _ in range(n)]
-
-primary_sum = sum(matrix[i][i] for i in range(n))
-secondary_sum = sum(matrix[i][n - i - 1] for i in range(n))
-
-print(abs(primary_sum - secondary_sum))
--- Python_Advanced/Multidimensional Lists - Exercise 1/matrix_of_palindromes.py
+++
@@ -1,6 +0,0 @@
-rows, cols = map(int, input().split())
-
-matrix = [[f"{chr(97 + r)}{chr(97 + r + c)}{chr(97 + r)}" for c in range(cols)] for r in range(rows)]
-
-for row in matrix:
-    print(" ".join(row))
